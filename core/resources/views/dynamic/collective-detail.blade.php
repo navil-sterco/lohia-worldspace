39da877a3c504d561b9923c721fc7bb21435965e
anupam-sterco <anupammishra.stercodigitex@gmail.com>
2026-09-18 10:24:18 +0530

    changes

--- core/resources/views/dynamic/collective-detail.blade.php
+++ core/resources/views/dynamic/collective-detail.blade.php
@@ -6,21 +6,21 @@
             <div class="prjtdtl_left">
                 <div class="proj_victor">
                     <img src="{{ asset('frontend-assets/images/victor-dash18.svg') }}" alt="victor"
-                        class="img-fluid w-100">
+                        class="img-fluid w-100 reveal-top">
                 </div>
                 <div class="prjtdtl_title">
-                    <h1 class="title21">Collective</h1>
-                    <h2 class="title72">{!! $collectiveDetail['detail']['name'] ?? '' !!}</h2>
+                    <h1 class="title21" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1200">Collective</h1>
+                    <h2 class="title72" data-aos="fade-up" data-aos-delay="300" data-aos-duration="1200">{!! $collectiveDetail['detail']['name'] ?? '' !!}</h2>
                 </div>
                 <div class="prjtdtl_nav">
                     <ul>
-                        <li><a href="#overview">Overview</a></li>
-                        <li><a href="#offer">Offer</a></li>
-                        <li><a href="#collaborators">Collaborators</a></li>
-                        <li><a href="#project-gallery">Gallery</a></li>
+                        <li data-aos="fade-up" data-aos-delay="300" data-aos-duration="1200"><a href="#overview">Overview</a></li>
+                        <li data-aos="fade-up" data-aos-delay="300" data-aos-duration="1200"><a href="#offer">Offer</a></li>
+                        <li data-aos="fade-up" data-aos-delay="300" data-aos-duration="1200"><a href="#collaborators">Collaborators</a></li>
+                        <li data-aos="fade-up" data-aos-delay="300" data-aos-duration="1200"><a href="#project-gallery">Gallery</a></li>
                     </ul>
                 </div>
-                <div class="prjtdtl_btns">
+                <div class="prjtdtl_btns" data-aos="fade-up" data-aos-delay="400" data-aos-duration="1200">
                     <a href="{{ url('contact-us') }}" class="request_btn">Enquire Now</a>
                 </div>
             </div>
